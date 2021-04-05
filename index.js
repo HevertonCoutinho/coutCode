9c0c8eb3acc504e384f5f65756444e1fe6a1bab4
Heverton <hevertonescudeiro@gmail.com>
2021-04-05 12:30:17 -0300

    Desenvolvimento de site responsivo sem bootstrap.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function verificarTexto(){
+    if (form.name.value == ""){
+        alert ("Preencha todos os campos do formulário");
+    }else{
+        alert ("Formulario preenchido com sucesso");
+    }
+}
